713170cebb1012ef6c10b001dbcd2074ee74c398
Juanes Sepulveda <jsepulveda@porcicarnes.com>
2025-11-13 16:27:23 -0500

    Adding the secret use for the dev management (pending to use aws secret manager)

--- Geocontrol_PPI_NET_9.AppHost/AppHost.cs
+++ Geocontrol_PPI_NET_9.AppHost/AppHost.cs
@@ -1,8 +1,9 @@
 try
 {
+    /// Create the builder for the application
     var builder = DistributedApplication.CreateBuilder(args);
-    // $"{ReferenceExpression.Create($"Endpoint=http://sql-pass;key={sqlPassword}")}"
-
+    
+    /// Define the docker behaviour component
     builder.AddDockerComposeEnvironment("env")
                .WithDashboard(dashboard =>
                {
@@ -17,34 +18,34 @@ try
     //    //.WithArgs(["-p 1433:1433", "-e MSSQL_SA_PASSWORD=S0p0rt3SQL"])
     //    .WithLifetime(ContainerLifetime.Persistent);
 
+    /// Assign the values secrets params
+    var sqlUserParam = builder.AddParameter("sql-user", secret: true);
     var sqlPasswordParam = builder.AddParameter("sql-pass", secret: true);
     var sqlConnectionParam = builder.AddParameter("sql-connection", secret: true);
+    var sqlDatabaseParam = builder.AddParameter("sql-database", secret: true);
 
-    var sql = builder.AddConnectionString("sql-connection-string");
+    /// Assign the value of the connection string in base of the secret value
+    var sql = builder.AddConnectionString("sql-connection-string", 
+        ReferenceExpression.Create($"Server={sqlConnectionParam};User Id={sqlUserParam};Password={sqlPasswordParam};TrustServerCertificate=True;Initial Catalog={sqlDatabaseParam};"));
+    // $"{ReferenceExpression.Create($"Endpoint=http://sql-pass;key={sqlPassword}")}"
 
     //var sqldDacpacProject = builder.AddSqlProject("sql-geocontrol-project")
     //   .WithDacpac("../Geocontrol_PPI_NET_9.Resources/GeoControl.dacpac")
     //   .WithReference(sql);
-
-    //var sqlContainer = builder.AddContainer("geocontrol-sql", "mcr.microsoft.com/mssql/server:2022-latest")
-    //    .WithEnvironment("ACCEPT_EULA", "Y")
-    //    .WithEnvironment("MSSQL_SA_PASSWORD", "S0p0rt3SQL")
-    //    .WithEnvironment("MSSQL_PID", "Express")
-    //    .WithBindMount("./bd_data", "/var/opt/mssql/data")
-    //    .WithVolume("geocontrol_sql_data", "/var/opt/mssql")
-    //    .WithEndpoint(1433, 1433)
-    //    .WithLifetime(ContainerLifetime.Persistent);
     #endregion
 
+    /// Assign the cache project manager
     var cache = builder.AddRedis("cache")
         .WaitFor(sql);
 
+    /// Assign the API service project
     var apiService = builder.AddProject<Projects.Geocontrol_PPI_NET_9_ApiService>("apiservice")
         .WithHttpHealthCheck("/health")
         .WithReference(sql)
         .WaitFor(cache)
         .WaitFor(sql);
 
+    /// Assign the web front-end project
     builder.AddProject<Projects.Geocontrol_PPI_NET_9_Web>("webfrontend")
         .WithExternalHttpEndpoints()
         .WithHttpHealthCheck("/health")
@@ -53,10 +54,11 @@ try
         .WithReference(apiService)
         .WaitFor(apiService);
 
+    /// Build and run the application
     builder.Build().Run();
 }
 catch (Exception ex)
 {
-    // Handle configuration errors
+    /// Handle configuration errors
     Console.WriteLine($"Database configuration failed: {ex.Message}");
 }
